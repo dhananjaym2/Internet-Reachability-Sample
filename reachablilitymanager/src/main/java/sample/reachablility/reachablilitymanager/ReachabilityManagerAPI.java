a3d40ac5de9c60baf4eda967b57b5326c5f5451d
dhananjay <dhananjaymohnot@gmail.com>
2019-05-04 00:41:33 +0530

    Check URL validity and internet connectivity before checking  connectivity to URL.

--- reachablilitymanager/src/main/java/sample/reachablility/reachablilitymanager/ReachabilityManagerAPI.java
+++ reachablilitymanager/src/main/java/sample/reachablility/reachablilitymanager/ReachabilityManagerAPI.java
@@ -1,11 +1,12 @@
 package sample.reachablility.reachablilitymanager;
 
+import android.content.Context;
 import sample.reachablility.reachablilitymanager.constants.AppConstants;
 
 public class ReachabilityManagerAPI {
-  public static String isInternetUrlReachable(String url) {
+  public static String isInternetUrlReachable(String url, Context context) {
     if (!url.isEmpty()) {
-      if (Internet.isUrlReachable(url)) {
+      if (Internet.isUrlReachable(url, context)) {
         return AppConstants.REACHABLE;
       }
     } else {
