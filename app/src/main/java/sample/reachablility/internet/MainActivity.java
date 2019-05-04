44fd3a26ddbe900754f5a27cef5dfea1813f70d2
dhananjay <dhananjaymohnot@gmail.com>
2019-05-04 13:29:39 +0530

    include library module to our project :reachablilitymanager. Demo usage of our library in a project. Remove static call.

--- app/src/main/java/sample/reachablility/internet/MainActivity.java
+++ app/src/main/java/sample/reachablility/internet/MainActivity.java
@@ -2,6 +2,7 @@ package sample.reachablility.internet;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import sample.reachablility.reachablilitymanager.ReachabilityManagerAPI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    String url = "http://www.google.com";
+    // demo usage of our library
+    ReachabilityManagerAPI reachabilityManagerAPI = new ReachabilityManagerAPI();
+    reachabilityManagerAPI.isInternetUrlReachable(url, this);
   }
 }
--- reachablilitymanager/src/main/java/sample/reachablility/reachablilitymanager/ReachabilityManagerAPI.java
+++ reachablilitymanager/src/main/java/sample/reachablility/reachablilitymanager/ReachabilityManagerAPI.java
@@ -4,7 +4,14 @@ import android.content.Context;
 import sample.reachablility.reachablilitymanager.constants.AppConstants;
 
 public class ReachabilityManagerAPI {
-  public static String isInternetUrlReachable(String url, Context context) {
+
+  /**
+   * Checks if the Internet network URL is reachable or not
+   *
+   * @param url URL as a {@link String}
+   * @return true if the url is reachable else false
+   */
+  public String isInternetUrlReachable(String url, Context context) {
     if (!url.isEmpty()) {
       if (Internet.isUrlReachable(url, context)) {
         return AppConstants.REACHABLE;
